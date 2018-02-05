266cb2ec42d06e890958d518dd6f6e7c3cfb4be7
Derek.S <derekseli@outlook.com>
2018-02-05 11:27:02 +0800

    add image size config

--- graph.py
+++ graph.py
@@ -7,6 +7,15 @@
 
 import xlrd
 import networkx as nx
+import matplotlib.pyplot as plt
+import codecs
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+
+plt.rcParams['font.sans-serif'] = ['SimHei']
+plt.rcParams['axes.unicode_minus'] = False
 
 # open excel
 def exceldata(filename, n):
@@ -46,19 +55,36 @@ def relation(filename):
         for r in range(sheet.nrows):
             cmp = sheet.cell(r, 0).value
             share = sheet.cell(r, 1).value
+            shares = []
+            for word in (share.split(",")):
+                if word != "" and word != "暂无":
+                    shares.append(word)
             if r != 0:
-                relations.setdefault(cmp, share)
-        print("去空前：", len(relations))
+                relations.setdefault(cmp, shares)
+        print("处理前：", len(relations))
         for key in relations.keys():
             if(len(relations[key]) == 0):
                 keys.append(key)
         for empty_key in keys:
             relations.pop(empty_key)
-        print("去空后：", len(relations))
+        print("处理后：", len(relations))
         return relations
     except Exception as e:
         print(e)
         return None
+
+# relations dict to tuples
+def coverelation(relations):
+    """
+    cover relations dict to tuples
+    :param relations: relations dict
+    :return: tuples
+    """
+    relation_dicts = []
+    for a in relations.keys():
+        for word in relations[a]:
+            relation_dicts.append((a, word))
+    return relation_dicts
 
 
 # clean data
@@ -98,12 +124,38 @@ def cleandata(datalist, dictname, splitstat):
 
 # create network graph
 def creategraph(picname, relation, dict1, dict2):
-    pass
+    colors = ['red', 'green', 'blue', 'yellow']
+    cmpnode = []
+    sharenode = []
+    with open(dict1, "r", encoding="utf8") as f1:
+        for node in f1.readlines():
+            cmpnode.append(node.split("\n")[0])
+    f1.close()
+    with open(dict2, "r", encoding="utf8") as f2:
+        for node in f2.readlines():
+            sharenode.append(node.split("\n")[0])
+    f2.close()
+    DG = nx.DiGraph()
+    print("节点较多，需要一定时间运行")
+    DG.add_nodes_from(cmpnode)
+    DG.add_nodes_from(sharenode)
+    DG.add_edges_from(relation)
+    #DG.add_nodes_from(['哈哈'])
+    #DG.add_nodes_from(['呵呵'])
+    #DG.add_edges_from([('哈哈', '呵呵')])
+    nx.draw(DG, with_labels=True, node_size=300, font_size=8, node_color=colors)
+    fig = plt.gcf()
+    fig.set_size_inches(30, 30)
+    fig.savefig(picname, dpi=600)
 
 
 if __name__ == "__main__":
-    # cmp_list_raw = exceldata("graph.xlsx", 0)
-    # shareholder_list_raw = exceldata("graph.xlsx", 1)
-    # cleandata(cmp_list_raw, "cmpdict.txt", False)
-    # cleandata(shareholder_list_raw, "shareholder.txt", True)
-    relations = relation("graph.xlsx")
+    excelfname = "graph.xlsx"
+    cmp_list_raw = exceldata(excelfname, 0)
+    shareholder_list_raw = exceldata(excelfname, 1)
+    cleandata(cmp_list_raw, "cmpdict.txt", False)
+    cleandata(shareholder_list_raw, "shareholder.txt", True)
+    relations = relation(excelfname)
+    # relation("graph.xlsx")
+    relations_dict = coverelation(relations)
+    creategraph("pic.png", relations_dict, "cmpdict.txt", "shareholder.txt")
