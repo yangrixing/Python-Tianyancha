0777e9dc1f2b575e221624e3d8aaaa59f39250ff
Derek.S <derekseli@outlook.com>
2018-02-07 13:01:20 +0800

    update analyze code

--- analyze.py
+++ analyze.py
@@ -6,6 +6,10 @@
 # @File    : analyze.py
 
 import xlrd
+from openpyxl.workbook import Workbook
+import arrow
+
+
 
 def exceldata(filename, n):
     """
@@ -45,14 +49,57 @@ def cleandata(sheet, datalist, dictname, splitstate):
                     for word in col:
                         if word != "" and word != "暂无":
                             data.append(word)
-            data_uniq = sorted(set(data), key=data.index)
             print("信息去重")
+            data_uniq = sorted(set(data), key=data.index)
             for word in data_uniq:
                 F.writelines(word + "\n")
+        else:
+            print("处理不带分隔符信息")
+            for datarow in range(sheet.nrows):
+                if datarow != 0:
+                    col = sheet.cell(datarow, datalist).value
+                    if col != "" and col != "暂无":
+                        F.writelines(col + "\n")
+    print("处理完成，关闭文件")
     F.close()
 
+
+def analyze(dictfile, sheet, row, outputfile, sheetkeys, headers):
+    keys = []
+    with open(dictfile, "r") as R:
+        for dictrow in R.readlines():
+            keys.append(dictrow.split("\n")[0])
+    R.close()
+    wb = Workbook()
+    ws = wb.active
+    ws.append(headers)
+
+    for dictkey in keys:
+        count = 0
+        resultstr = ""
+        for datarow in range(sheet.nrows):
+            if datarow != 0:
+                col = sheet.cell(datarow, row).value
+                if dictkey in col:
+                    for rowkey in sheetkeys:
+                        resultstr += (sheet.cell(datarow, rowkey).value + ",") # 获取符合条件的对应列数据
+                        count += 1
+        result = [dictkey, count, resultstr]
+        ws.append(result)
+    wb.save(filename=outputfile)
 
 
 if __name__ == "__main__":
     sheet = exceldata("result.xlsx", 0)
-    cleandata(sheet, 16, "share_dict.txt", True)
+    # 股东信息字典生成
+    # cleandata(sheet, 16, "share_dict.txt", True)
+    # 法人名称字典
+    # cleandata(sheet, 2, "legal_dict.txt", False)
+    # 对外投资字典
+    # cleandata(sheet, 17, "invest_dict.txt", True)
+    # 公司名称
+    # cleandata(sheet, 1, "cmp_dict.txt", False)
+    # 分析
+    sheetkeyslist = [0]
+    sheetheader = ['股东名称', '参股公司数量', '参股公司名单']
+    analyze("share_dict.txt", sheet, 16, "test.xlsx", sheetkeyslist, sheetheader)
